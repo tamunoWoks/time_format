4e09f75e2e41a9d86ff6f16d1a0a741f4959a47c
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-07 13:16:46 -0600

    Convert the files if the input matches the pattern, extract matched groups and convert using another function

--- format.py
+++ format.py
@@ -11,6 +11,12 @@ def convert(s):
     #Use the regex to match the entire input string
     match = re.search(rf'^[regex] to {regex}$', s)
 
+    #if the input matches pattern, convert the times
+    if match:
+        #Extract matched groups and convert using 'standard' function
+        start_time = standard(match.group(1), match.group(2), match.group(3))
+        end_time = standard(match.group(4), match.group(5), match.group(6))
+
 
 
 if __name__ == '__main__':
