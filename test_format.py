f8ffa38b9f52034fb29a257a85e38435eb3f5882
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-07 13:38:38 -0600

    Update test_convert() in test_format.py

--- test_format.py
+++ test_format.py
@@ -3,7 +3,10 @@ from format import convert
 
 
 def test_convert():
-    ...
+    assert convert('9 AM to 5 PM') == '09:00 to 17:00'
+    assert convert('9:00 AM to 5:00 PM') == '09:00 to 17:00'
+    assert convert('10 PM to 8 AM') == '22:00 to 08:00'
+    assert convert('10:30 PM to 8:50 AM') == '22:30 to 08:50'
 
 
 def test_value_error():
