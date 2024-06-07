072074eadd168a43bd4f34b1814538a60fc52f33
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-07 13:46:32 -0600

    Clean up test_format.py

--- test_format.py
+++ test_format.py
@@ -3,18 +3,18 @@ from format import convert
 
 
 def test_convert():
-    assert convert('9 AM to 5 PM') == '09:00 to 17:00'
-    assert convert('9:00 AM to 5:00 PM') == '09:00 to 17:00'
-    assert convert('10 PM to 8 AM') == '22:00 to 08:00'
-    assert convert('10:30 PM to 8:50 AM') == '22:30 to 08:50'
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("10 PM to 8 AM") == "22:00 to 08:00"
+    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
 
 
 def test_value_error():
     with pytest.raises(ValueError):
-        convert('9AM - 5PM')
+        convert("9AM - 5PM")
     with pytest.raises(ValueError):
-        convert('09:00 to 17:00')
+        convert("09:00 to 17:00")
     with pytest.raises(ValueError):
-        convert('15:00 AM to 25:00 PM')
+        convert("15:00 AM to 25:00 PM")
     with pytest.raises(ValueError):
-        convert('9:60 AM to 5:60 PM')
+        convert("9:60 AM to 5:60 PM")
