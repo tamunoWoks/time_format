a3f3107c20780299287dfdf6d2d39cf9813ae1b4
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-07 13:11:01 -0600

    Create func convert(), define regex and use regex to match input string

--- format.py
+++ format.py
@@ -4,6 +4,15 @@ def main():
     ...
 
 
+def convert(s):
+    #define the regex to match the time format 'hh:mm AM/PM'
+    regex = r'(0?[1-9]|1[0-2]):?([0-5][0-9])? (AM|PM)'
+
+    #Use the regex to match the entire input string
+    match = re.search(rf'^[regex] to {regex}$', s)
+
+
+
 if __name__ == '__main__':
     main()
 
